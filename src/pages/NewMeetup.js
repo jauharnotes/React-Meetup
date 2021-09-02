bb31f6dcccf588f9acd4b64aeb4ad19a97d1ef52
Jauharuddin <jauharnotes@gmail.com>
2021-09-02 19:29:01 +0700

    fetch API

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
 
 function NewMeetupPage() {
+  const history = useHistory();
+  function addMeetupHandler(meetuoData) {
+    fetch(
+      "https://react-meetup-5d446-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetuoData),
+        header: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <div>
       <h2>This New Meetup Page</h2>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </div>
   );
 }
